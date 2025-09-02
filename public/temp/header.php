7bf87611925059b36b408e1834edc767875ede2b
saran-decoder <saranmass685@gmail.com>
2025-09-02 13:43:28 +0530

    small change fix some issues

--- public/temp/header.php
+++ public/temp/header.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-white sticky-top">
     <div class="container">
-        <a class="navbar-brand text-primary" href="index.php">
+        <a class="navbar-brand text-primary" href="./">
             TNBooking.in
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
@@ -9,10 +9,10 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav mx-auto">
                 <li class="nav-item">
-                    <a class="nav-link active" href="index.php">Home</a>
+                    <a class="nav-link active" href="./">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="booking.php">Booking</a>
+                    <a class="nav-link" href="booking">Booking</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">Support</a>
